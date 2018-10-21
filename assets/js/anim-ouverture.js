632960d1517cebf0e39ddfba17e479714d1899cb
hauw <hauw.dominique@gmail.com>
2018-10-21 17:06:30 +0200

    test animations

---
+++ assets/js/anim-ouverture.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    $('.btn').click(function () {
+        $('.btn').css({
+            'animation-name': 'openWindow',
+            'animation-duration': '3s'
+        });
+    });
+
+
+    // $('button').click(function () {
+    //     $('#bigCircle').css({
+    //         'animation-name': 'rotateRoue',
+    //         'animation-iteration-count': '2',
+    //         'animation-duration': '2.5s',
+    //         'animation-direction': 'alternate'
+    //     });
+    // });
+
+
+
+
+});
---
+++ assets/js/animate_roue.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+$('button').click(function (){
+$('#bigCircle').css({
+    'animation-name' : 'rotateRoue',
+    'animation-iteration-count': '2',
+    'animation-duration': '2.5s',
+    'animation-direction': 'alternate'
+  });
+});
+
+  });
